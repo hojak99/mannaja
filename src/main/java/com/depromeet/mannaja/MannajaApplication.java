296af1e2dc895f6dead6be5c0973bfbb9f4148db
mumu <ramason@coupang.com>
2018-10-16 20:27:23 +0900

    스프링부트 initializer

---
+++ src/main/java/com/depromeet/mannaja/MannajaApplication.java
@@ -0,0 +1,12 @@
+package com.depromeet.mannaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannajaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MannajaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/depromeet/mannaja/MannajaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.depromeet.mannaja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MannajaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
