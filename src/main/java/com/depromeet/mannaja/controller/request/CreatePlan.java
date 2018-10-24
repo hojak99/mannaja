c83cb843e807f75dfc08c79a0cb649b738423323
hojak99 <hojak99@gmail.com>
2018-10-25 01:52:01 +0900

    약속 생성 시 yyyy-MM-dd 로 생성되는 버그 수정

--- src/main/java/com/depromeet/mannaja/controller/request/CreatePlan.java
+++ src/main/java/com/depromeet/mannaja/controller/request/CreatePlan.java
@@ -1,9 +1,12 @@
 package com.depromeet.mannaja.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 @Getter
 public class CreatePlan implements Serializable {
